f1bb37799fe3241b238771411cc889c250157730
Emprario <pierre.cameleri@free.fr>
2023-08-28 22:34:36 +0200

    redifine struc

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 ### First init ###
 
+from typing import Any, Coroutine
 import discord
 from discord.ext.commands import Bot
 
@@ -14,26 +15,54 @@ bot = Bot('µ ',intents=intents)
 
 ### Stages and functions ###
 
-class Stage1:
-    """CGU aggrements"""
-    pass
+async def SETUP_VARS ():
+    global CHANNEL_ROLES
+    global SERVER
+    
+    global MSG_CGU
+    global MSG_CLASSE
+    global MSG_ROLES
 
-class Stage2:
-    """Classe roles"""
-    pass
+    global REAC_CGU
+    global REAC_CLASSE
+    global REAC_ROLES
 
-class Stage3:
-    """Channel roles"""
-    pass
+    global REACT_DICT
 
-class User_pystatus:
-    """backend User object"""
-    pass
+    CHANNEL_ROLES = bot.get_channel(1143999446070337559)
+    SERVER = bot.get_guild(1143931284595417140)
+    
+    MSG_CGU = await CHANNEL_ROLES.fetch_message(1143999517922963466)
+    REAC_CGU = MSG_CGU.reactions()
+    
+    MSG_CLASSE = await CHANNEL_ROLES.fetch_message(1144010479145070673)
+    REAC_CLASSE = MSG_CLASSE.reactions()
+    
+    MSG_ROLES = await CHANNEL_ROLES.fetch_message(1144014029875060897)
+    REAC_ROLES = MSG_ROLES.reactions()
+
+    INTERN = SERVER.get_role(1143936334986223789)
+    EXTERN = SERVER.get_role(1143937761666158724)
+    REACT_DICT: dict[str,discord.Role] = {
+        "✅":SERVER.get_role(1143994418559455312),
+        "<:L1:1144016256001904660>":INTERN,
+        "<:L1_plus:1144014333446193275>":INTERN,
+        "<:L2:1144016303762460672>":EXTERN,
+        "#️⃣":SERVER.get_role(1144017617938554970),
+        "📕":SERVER.get_role(1144017476074602627),
+        "🎮":SERVER.get_role(1144012929541357578)
+    }
+
+class User_pystatus (Common):
+    self.user_id = user_id
+    self.member = discord.utils.get(self.SERVER.members, id=user_id)
+    self.reactions = [user async for user in self.reactions.users()] #if user == user_id]
+    print(self.reactions)
 
 def update_react(user:User_pystatus):
     pass
 
-def add_role(user:User_pystatus):
+def add_role(user:User_pystatus, pretend: discord.Reaction):
     pass
 
 def remove_role(user:User_pystatus):
@@ -51,6 +80,7 @@ async def on_ready():
     print("Username: %s"%bot.user.name)
     print("ID: %s"%bot.user.id)
     print("----------------------")
+    SETUP_VARS()
 
 @bot.event
 async def on_message(ctx):
